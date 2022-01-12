666fb7e72567f0e912a5c6eecb3dafd64c909b80
Emma Grey <emgrey02@gmail.com>
2022-01-11 20:41:54 -0500

    Add a focus circle to first scene

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Manager } from './Manager';
 import { LoaderScene } from './scenes/LoaderScene';
 
-Manager.initialize(640, 480, 0x333333);
+Manager.initialize(640, 480, 0x111111);
 
 const loady: LoaderScene = new LoaderScene();
 Manager.changeScene(loady);
--- src/scenes/First.ts
+++ src/scenes/First.ts
@@ -1,4 +1,4 @@
-import { Sprite } from 'pixi.js';
+import { Sprite, /* filters, */ Graphics } from 'pixi.js';
 import { Group, Tween } from 'tweedle.js';
 import { Manager } from '../Manager';
 import { GameScene } from './GameScene';
@@ -6,6 +6,7 @@ import { GameScene } from './GameScene';
 export class First extends GameScene {
   private crown: Sprite;
   private bg: Sprite;
+  private circle: Graphics;
 
   constructor() {
     super();
@@ -16,12 +17,20 @@ export class First extends GameScene {
     this.bg.zIndex = -1;
     this.addChild(this.bg);
 
+    this.circle = new Graphics()
+      .beginFill(0xff0000)
+      .drawCircle(0, 0, 100)
+      .endFill();
+    // this.circle.filters = [new filters.BlurFilter()];
+    this.addChild(this.circle);
+    this.bg.mask = this.circle;
+
     this.crown = Sprite.from('crown');
     this.crown.anchor.set(0.5);
-    this.crown.scale.set(0.2);
+    this.crown.scale.set(0.03);
     this.crown.x = 100;
     this.crown.y = 100;
-    this.addChild(this.crown);
+    this.bg.addChild(this.crown);
 
     new Tween(this.crown)
       .to({ y: 105 }, 1000)
@@ -38,5 +47,12 @@ export class First extends GameScene {
     if (this.checkCollision(this.player, this.crown)) {
       Manager.changeScene(new GameScene());
     }
+
+    this.updateCirclePosition(this.player.x, this.player.y);
+  }
+
+  updateCirclePosition(x: number, y: number) {
+    this.circle.x = x;
+    this.circle.y = y;
   }
 }
